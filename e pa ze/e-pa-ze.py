387b619d9c5ef2f4e382436eb92c7203a4b4096f
Leotrim Shatri <77514315+leotrimshatri@users.noreply.github.com>
2021-10-29 23:10:20 +0200

    Update e-pa-ze.py

--- e pa ze/e-pa-ze.py
+++ e pa ze/e-pa-ze.py
@@ -1,1 +1,16 @@
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "w":
+            if letter.isupper():
+                translation = translation + "Ë"
+            else:
+                translation = translation + "ë"
 
+
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("Enter a phrase: ")))
